74ed393e22cec5e2aa5809c01d519731b8c87793
EVroup <Vsabanov19@gmail.com>
2022-10-06 20:27:30 +0300

    pr

---
+++ RUN/Assets/Scripts/spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+public GameObject spawm;
+
+void Start()
+{
+DoIt();
+}
+
+void DoIt()
+{
+          Vector3 randomSpawnPosition = new Vector3(Random.Range(-50,50), 5, Random.Range(-50, 50));
+            Instantiate(spawm, randomSpawnPosition, Quaternion.identity);
+          { Invoke("DoIt", 1f);}
+}
+}
+      
+  
